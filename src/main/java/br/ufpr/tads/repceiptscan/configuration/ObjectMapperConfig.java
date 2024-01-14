a75064e25bd4b9fed893a50b293ca78215f67828
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-01-14 20:18:07 -0300

    Add objectMapper configuration to (des)serialize Java 8 Date/Time

---
+++ src/main/java/br/ufpr/tads/repceiptscan/configuration/ObjectMapperConfig.java
@@ -0,0 +1,20 @@
+package br.ufpr.tads.repceiptscan.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+}
