488259f6e8061ed8ed8d0cb913386e762dd30201
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-08-28 19:55:52 -0300

    Refactor: Create HtmlUtils class and move utility methods

---
+++ src/main/java/br/ufpr/tads/repceiptscan/utils/HtmlUtils.java
@@ -0,0 +1,29 @@
+package br.ufpr.tads.repceiptscan.utils;
+
+import br.ufpr.tads.repceiptscan.mapper.ElementNotFoundException;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Optional;
+
+public class HtmlUtils {
+
+    public static Element getElement(Document document, String select) {
+        return Optional.ofNullable(document.select(select).first())
+                .orElseThrow(() -> new ElementNotFoundException("Element not found: " + select));
+    }
+
+    public static Element getElement(Element element, String select) {
+        return Optional.ofNullable(element.select(select).first())
+                .orElseThrow(() -> new ElementNotFoundException("Element not found: " + select));
+    }
+
+    public static Elements getElements(Document document, String select) {
+        return Optional.ofNullable(document.select(select).first())
+                .map(Element::children)
+                .orElseThrow(() -> new ElementNotFoundException("Element not found or has no children: " + select));
+    }
+
+}
+
